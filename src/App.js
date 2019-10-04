eb1aae83b66426a369bef2c0043cd845f1d5a18c
Ashutosh Kumar Singh <ashusingh15673@gmail.com>
2019-10-04 11:06:29 +0530

    debugging

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import { getStoryIds } from "./services/hnApi";
 
 function App() {
   const [storyIds, setStoryIds] = useState([]);
+
+  useEffect(() => {
+    getStoryIds();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">Hello React</header>
+      <p>{storyIds}</p>
     </div>
   );
 }
--- src/services/hnApi.js
+++ src/services/hnApi.js
@@ -1,12 +1,11 @@
 import axios from "axios";
-import { async } from "q";
 
 export const baseUrl = "https://hacker-news.firebaseio.com/v0/";
 export const newStoriesUrl = `${baseUrl}newStories.json`;
 export const storyUrl = `${baseUrl}item/`;
 
 export const getStoryIds = async () => {
-  const result = await axios.get(newStoriesUrl.then(data => data));
+  const result = await axios.get(newStoriesUrl).then(data => data);
 
   return result;
 };
