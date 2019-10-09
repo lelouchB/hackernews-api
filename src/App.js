3644be6a475a93a7cc51f7051621f3ce2aa50643
Ashutosh Kumar Singh <ashusingh15673@gmail.com>
2019-10-09 19:36:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import { StoriesContainer } from "./containers/StoriesContainer";
 
-export const App = () => (
+export default const App = () => (
   <p>
     <StoriesContainer />
   </p>
